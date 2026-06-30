2fde71cbc1799aea043760f42ee8c71a1e186dbc
harryspeed23-lab <harryspeed23@gmail.com>
2026-06-30 13:24:19 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
-import os
+from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
 BOT_TOKEN = "8657559322:AAE3KDwV092R6w-tqDoz1X87sB5MpBpn9Vw"
 
 async def auto_reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message:
-        await update.message.reply_text("မင်္ဂလာပါ 👋 Auto Reply Bot ဖြစ်ပါတယ်။")
+        await update.message.reply_text("မင်္ဂလာပါ 🤖 Auto Reply Bot")
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, auto_reply))
+app = Application.builder().token(BOT_TOKEN).build()
+
+app.add_handler(
+    MessageHandler(filters.TEXT & ~filters.COMMAND, auto_reply)
+)
 
 print("Bot is running...")
 app.run_polling()
