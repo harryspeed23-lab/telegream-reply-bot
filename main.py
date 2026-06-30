23b418d190f9e17f7fb236de82420c9a6110bbe8
harryspeed23-lab <harryspeed23@gmail.com>
2026-06-30 13:36:24 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from telegram import Update
 from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
-BOT_TOKEN = "8657559322:AAE3KDwV092R6w-tqDoz1X87sB5MpBpn9Vw"
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def auto_reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message:
         await update.message.reply_text("မင်္ဂလာပါ 🤖 Auto Reply Bot")
-
+token("BOT_TOKEN")
 app = Application.builder().token("8657559322:AAE3KDwV092R6w-tqDoz1X87sB5MpBpn9Vw").build()
 
 app.add_handler(
